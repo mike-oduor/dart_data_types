c035348392718971edde9183484de219419fbde5
mike <mikeoduori12345@gmail.com>
2024-03-17 16:48:51 +0300

    initial commit

---
+++ data_types1.dart
@@ -0,0 +1,23 @@
+void main() {
+  //This program demonstrates different data types in dart
+  //variable declarations.
+  //Number data type; for declaration of numeric values.
+  num age = 20;
+  //Integer data type. For declaration of integers.
+  int num1 = 8;
+  //String data type. Used to declare stringed data.
+  String first_name = "Michael.";
+  //double data type. used to declare numbers in double precision.
+  double num2 = 20.54;
+  //boolean data type. used to determine true or false.
+  bool isMarried = false;
+  // Outputs.
+  print("Age is $age");
+  print("Number is $num1");
+  print("First name is $first_name");
+  print("Double number is $num2");
+  print("Marital status is $isMarried");
+  //maps. A map describes paired collections having keys and values.
+  var map = {'position1': 'student', 'position2': 'Engineer'};
+  print(map);
+}
